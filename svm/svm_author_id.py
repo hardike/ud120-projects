9942a6171ae6754af7e94da30cae9d9f44fc0c18
hardike <hardike@gmail.com>
2015-02-17 20:54:55 -0800

    SVM exercise completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 2 (SVM) mini-project
 
     use an SVM to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,7 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel = 'linear')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-t1, 3), "s"
+
+
+print('Accurancy is:')
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
 
 #########################################################
-
-
